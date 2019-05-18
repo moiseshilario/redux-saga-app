99ff988c4e7a13b9d8feec970280a5ec8e7ed067
moiseshilario <moiseshilariu@gmail.com>
2019-05-18 18:16:47 -0300

    Add basic todos app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import './config/ReactotronConfig';
 
 import store from './store';
+
+import TodoList from './components/TodoList';
 
 const App = () => (
   <Provider store={store}>
     <div>Hello</div>
+    <TodoList />
   </Provider>
 );
 
